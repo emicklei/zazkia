f258fb76d9bea4310377ceb12332826c40da857a
Ernest Micklei <ernest.micklei@gmail.com>
2016-11-21 08:33:14 +0100

    add stub for drone.io
    Change-Id: Ic93d7e0d7c3281d329ab89623f93a25ba5133de4

---
+++ asset_stub.go
@@ -0,0 +1,11 @@
+// +build droneio
+
+package main
+
+import "os"
+
+func Asset(name string) (data []byte, err error) { return }
+
+func AssetDir(name string) (list []string, err error) { return }
+
+func AssetInfo(name string) (f os.FileInfo, err error) { return }
